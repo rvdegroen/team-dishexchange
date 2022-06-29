7db8d6767a113d94f4156c1a47735dd4bfb6e933
rvdegroen <suiyichiro@hotmail.com>
2022-06-29 22:13:29 +0200

    implemented gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,25 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
 
-exports.default = defaultTask;
+gulp.task("sass", () =>
+  // return sass files from the direct folders or subfolders (helpers)
+  gulp
+    .src("static/scss/**/*.scss")
+    // compile scss files to css and if it goes wrong, log the error
+    // compress compiled css files (source: https://www.npmjs.com/package/gulp-sass --> render with options)
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    // bundle the compiled compressed css files
+    .pipe(concat("bundle.min.css"))
+    // save the compiled css into the destination folder
+    .pipe(gulp.dest("static/css/"))
+);
+
+gulp.task("watch", () => {
+  // which files do I want to be watched and which tasks will I run after a change has been made?
+  gulp.watch("static/scss/**/*.scss", gulp.series(["sass"]));
+});
+
+// if you type gulp in terminal, it will run default
+// gulp series can run an array of tasks one after another: first run sass then something else if I wanted to add one in the future
+gulp.task("default", gulp.series(["sass"]));
